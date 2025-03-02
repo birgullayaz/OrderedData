43e73a71a100f4e1a88e4c11085c2d6a22c83491
Birgül <44294078+birgullayaz@users.noreply.github.com>
2025-03-02 03:58:20 +0300

    Update Controller.cs
    Main controller is updated

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -1,6 +1,21 @@
-namespace OrderedData.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using OrderedData.Data;
+using Microsoft.Extensions.Logging;
 
-public class  Controller
+namespace OrderedData.Controllers
 {
+    public abstract class Controller : Microsoft.AspNetCore.Mvc.Controller
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly ILogger<Controller> _logger;
 
- 0
+        protected Controller(ApplicationDbContext context, ILogger<Controller> logger)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            // Sadece sisteme giriş logu
+            _logger.LogInformation("System accessed at: {Time}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+} 
